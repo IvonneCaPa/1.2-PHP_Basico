ed951abb66216c21c0b0749b0d622af86b9ef34b
Ivonne Cantor <ivonnecp@gmail.com>
2025-01-27 18:06:35 +0100

    Sprint 1, tema 2, nivel 1 calculadora ok

--- calculadora.php
+++ calculadora.php
@@ -3,26 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Calculadora</title>
 </head>
 <body>
+    <h1>Calculadora</h1>
+    <div class="container">
     <form action="#" method="get">
         <fieldset>
-            <legend>Calculadora</legend>
+            <legend>Escriba el primer número <br> escoja la operación <br> escriba el segundo número</legend>
             <label for="num1">Número 1</label>
-            <input type="number" name="num1" id="">
+            <input type="number" name="num1" required><br>
             <select name="ope" id="">
                 <option value="+">+</option>
                 <option value="-">-</option>
                 <option value="*">*</option>
                 <option value="/">/</option>                
-            </select>
+            </select><br>
             <label for="num2">Número 2</label>
-            <input type="number" name="num2">
+            <input type="number" name="num2" min="1" required>
             <input type="submit" value="Igual">
         </fieldset>
     </form>
-    <div>
+    <div class="resultado">
     <?php
         $num1 = $_GET['num1'];
         $num2 = $_GET['num2'];
@@ -48,6 +51,8 @@
         echo "El resultado es: $result";
     ?>
     </div>
+    </div>
+
 
     
    
